9a57dae692b3644eeeacfe97be6cae3c761f6d9f
pseudocode21 <151240191+pseudocode21@users.noreply.github.com>
2025-10-04 02:42:56 +0530

    Navbar Done, Hero Section - Added the 3D Model, Setting the Position, rotation and scale using Leva

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./section/Navbar";
+import Hero from "./section/Hero";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/section/Hero.jsx
@@ -0,0 +1,92 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import HackerRoom from "../components/HackerRoom";
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants";
+const Hero = () => {
+  const x = useControls("HackerRoom", {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10,
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10,
+    },
+  });
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ minWidth: 441, maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 769, maxWidth: 1024 });
+
+  // console.log("isMobile:", isMobile);
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  return (
+    <section
+      className="min-h-screen w-full flex flex-col relative
+    "
+    >
+      <div className="w-full mx-auto flex flex-col sm:mt-30 mt-17 c-space gap-3 ">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Gaurav <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Buliding Products & Brands{" "}
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0 ">
+        <Leva />
+        <Canvas className="w-full h-full ">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              // scale={0.06}
+              // position={[0, 0, 0]}
+              // rotation={[0, 280, 0]}
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+              scale={sizes.deskScale}
+            />
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
