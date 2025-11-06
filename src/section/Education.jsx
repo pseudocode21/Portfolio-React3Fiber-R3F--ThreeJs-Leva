40df0fff093d6a7e9fb66bb6ca82d93b496b1e88
pseudocode21 <151240191+pseudocode21@users.noreply.github.com>
2025-11-07 02:41:40 +0530

    Adding the animated character to the my education section

--- src/section/Education.jsx
+++ src/section/Education.jsx
@@ -1,21 +1,46 @@
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense, useState } from "react";
 import { educationDetails } from "../constants";
+import { OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import Developer from "../components/Developer";
 
 const Education = () => {
+  const [animationName, setAnimationName] = useState("idle");
   return (
     <div className="c-space my-20">
       <div className="w-full text-white-600">
         <h3 className="head-text">My Education</h3>
         <div className="work-container">
           <div className="work-canvas">
-            <Canvas></Canvas>
+            <Canvas>
+              <ambientLight intensity={7} />
+              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+              <directionalLight position={[10, 10, 10]} intensity={1} />
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+              <Suspense fallback={<CanvasLoader />}>
+                <Developer
+                  position-y={-3}
+                  scale={3}
+                  animationName={animationName}
+                />
+              </Suspense>
+            </Canvas>
           </div>
           <div className="work-content">
             <div className="sm:py-10 py-5 sm:px-5 px-2.5">
               {educationDetails.map(
                 ({ id, name, pos, icon, duration, course, animation }) => (
-                  <div key={id} className="work-content_container group">
+                  <div
+                    key={id}
+                    className="work-content_container group"
+                    // ✅ This PASSES a function to be run on click
+                    onClick={() => setAnimationName(animation.toLowerCase())}
+                    onPointerOver={() =>
+                      setAnimationName(animation.toLowerCase())
+                    }
+                    onPointerOut={() => setAnimationName("idle")}
+                  >
                     <div className="flex flex-col h-full justify-start items-center py-2">
                       <div className="work-content_logo">
                         <img src={icon} alt="logo" className="w-full h-full" />
--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="c-space pt-7 pb-3 border-1 border-black-300 flex justify-between items-center flex-wrap gap-5">
+    <section className="c-space pt-7 pb-3 flex justify-center items-center flex-wrap gap-5 ">
       <div className="text-white-500 flex gap-2">
         <p>Terms & Conditions</p>
         <p>Privacy Policy </p>
